f314d7f1de8a8336cef3b4570f1f2b0237ae9ee3
alma_sanchez <alma08sanche@gmail.com>
2019-10-17 01:23:58 -0700

    Adds clock code and readme

---
+++ 02 - JS and CSS Clock/clock.js
@@ -0,0 +1,32 @@
+const secondHand= document.querySelector(".second-hand");
+const minuteHand= document.querySelector(".minute-hand");
+const hourHand= document.querySelector(".hour-hand");
+
+function setDate(){
+  const now = new Date();
+  const sec = now.getSeconds();
+  const min = now.getMinutes();
+  const hour = now.getHours() - 12;
+  
+  const offset = 90;
+  // seconds divided by 60 
+  const secDegrees = ((sec / 60) * 360) + offset;  
+  const minDegrees = ((360/60) * min) + offset;
+  const hourDegrees = ((360/12) * hour) + offset;
+  
+  // Stop the second hand from going all the way around to reach 0 and continue instead
+  if(sec == 0) {
+    console.log(secondHand);
+    secondHand.style.transition = "none";
+  }
+  if(secDegrees == 1) {
+    secondHand.style.transition = "all 0.05s";
+  }
+
+  secondHand.style.transform = `rotate(${secDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
---
+++ 02 - JS and CSS Clock/readme.md
@@ -0,0 +1,21 @@
+![screenshot_video.jpg](https://github.com/Alma-Sanchez/JavaScript30/blob/master/02%20-%20JS%20and%20CSS%20Clock/images/clock_image.jpg)
+
+Day 2: CSS & JS Clock
+=========
+__Overview__
+  -  
+  -
+
+
+
+__Extra Features Implemented__
+  - 
+  -
+  
+__Features TODO__
+
+
+
+
+<hr />
+To view the course and give it a try yourself visit: https://JavaScript30.com
